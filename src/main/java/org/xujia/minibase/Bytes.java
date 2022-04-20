c4e553083b94e13d41534b6ae113b88fe44c654c
caffeflow <1023101057@qq.com>
2022-04-20 14:12:46 +0800

    bytes code 100%

--- src/main/java/org/xujia/minibase/Bytes.java
+++ src/main/java/org/xujia/minibase/Bytes.java
@@ -1,7 +1,10 @@
 package org.xujia.minibase;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.format.ResolverStyle;
 
 public class Bytes {
 
@@ -37,39 +40,86 @@ public class Bytes {
     }
 
     public static String toHex(byte[] buf){
-        return null;
+        return toHex(buf,0,buf.length);
     }
 
     public static String toHex(byte[] buf, int offset, int len) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i=offset;i < offset+len;i++){
+            int x = buf[i];
+            if (32 < x && x < 127){
+                sb.append((char)x);
+            }else{
+                sb.append("\\x").append(HEX_TMP.charAt((x >> 4) & 0x0F)).append(HEX_TMP.charAt(x & 0x0F));
+            }
+        }
+        return sb.toString();
     }
 
     public static byte[] toBytes(byte[] a, byte[] b) {
-        return null;
-
+        if (a == null) return b;
+        if (b == null) return a;
+        byte[] result = new byte[a.length + b.length];
+        System.arraycopy(a,0,result,0,a.length);
+        System.arraycopy(b,0,result,a.length,b.length);
+        return result;
     }
 
     public static int toInt(byte[] a) {
-        return 0;
-
+        int x = 0;
+        for (int i=0;i<4;i++){
+            int j = (3-i) << 3;
+            x |= (a[i] << j) & (0xFF << j);
+        }
+        return x;
     }
 
     public static long toLong(byte[] a) {
-        return 0;
+        long x = 0;
+        for (int i = 0; i < 8; i++) {
+            int j = (7-i) << 3;
+            x |= ((long)a[i] << j) & (0xFFL << j);
+        }
+        return x;
     }
 
     public static byte[] slice(byte[] buf, int offset, int len) throws IOException {
-        return null;
-
+        if (buf == null){
+            throw new IOException("buffer is null");
+        }
+        if (offset < 0 || len < 0){
+            throw new IOException("Invalid offset:"  + offset  + "Invalid len:" + len);
+        }
+        if (offset + len > buf.length){
+            throw new IOException("Buffer overflow, offset:" + offset + "len:" + len + "buf.length:" + buf.length);
+        }
+        byte[] bytes = new byte[len];
+        System.arraycopy(buf,offset,bytes,0,len);
+        return bytes;
     }
 
     public static int hash(byte[] key) {
-        return 0;
-
+        if (key == null){
+            return 0;
+        }
+        int h = 1;
+        for (int i = 0; i < key.length; i++) {
+            h = (h << 5) + h + key[i];
+        }
+        return h;
     }
 
     public static int compare(byte[] a, byte[] b) {
-        return 0;
-
+        if (a == b) return 0;
+        if (a == null) return -1;
+        if (b == null) return 1;
+        for (int i=0,j=0;i<a.length && j < b.length;i++,j++){
+            int x = a[i] & 0xFF;
+            int y = a[i] & 0xFF;
+            if (x != y){
+                return x - y;
+            }
+        }
+        return a.length - b.length;
     }
 }
