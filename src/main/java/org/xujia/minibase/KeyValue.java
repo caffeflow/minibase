055f93e87f0a850443fff99e153e9830e886cda4
caffeflow <1023101057@qq.com>
2022-04-21 21:29:57 +0800

    KeyValue.java 30%

--- src/main/java/org/xujia/minibase/KeyValue.java
+++ src/main/java/org/xujia/minibase/KeyValue.java
@@ -22,13 +22,60 @@ public class KeyValue implements Comparable<KeyValue>{
     private Op op;
     private long sequenceId;
 
+    public KeyValue(byte[] key,byte[] value,Op op,long sequenceId){
+        assert key != null;
+        assert value != null;
+        assert op != null;
+        assert sequenceId > 0;
+        this.key = key;
+        this.value = value;
+        this.op = op;
+        this.sequenceId = sequenceId;
+    }
+
     public enum Op{
         Put((byte) 0),Delete((byte) 1);
         private byte code;
         Op(byte code){
             this.code = code;
         }
+        public static Op code2Op(byte code){
+            switch (code){
+                case 0 : return Put;
+                case 1 : return Delete;
+                default:
+                    throw new IllegalArgumentException("unknow code:" + code);
+            }
+        }
+
+        public byte getCode(){
+            return this.code;
+        }
     }
+
+    public static KeyValue create(byte[] key,byte[] value,Op op,long sequenceId){
+        return new KeyValue(key,value,op,sequenceId);
+    }
+
+    public static KeyValue createPut(byte[] key,byte[] value,long sequenceId){
+        return KeyValue.create(key,value,Op.Put,sequenceId);
+    }
+
+    public static KeyValue createDelete(byte[] key,long sequenceId){
+        return KeyValue.create(key,Bytes.EMPTY_BYTES,Op.Delete,sequenceId);
+    }
+
+    public byte[] getKey(){return key;}
+
+    public byte[] getValue(){return value;}
+
+    public Op getOp(){return op;}
+
+    public long getSequenceId(){return this.sequenceId;}
+
+    private int getRawKeyLen(){return key.length + OP_SIZE + SEQ_ID_SIZE;} // NOTE
+
+
 
     @Override
     public int compareTo(KeyValue kv) {
@@ -41,9 +88,6 @@ public class KeyValue implements Comparable<KeyValue>{
         return 0;
     }
 
-    @Override
-    public boolean equals(KeyValue kv) {
-        return false;
-    }
+
 
 }
