4de9254de835dbe00a79ddf39923a41893db8ac5
betterJia <1023101057@qq.com>
2022-04-25 11:30:11 +0800

    tmp

--- src/main/java/org/xujia/minibase/KeyValue.java
+++ src/main/java/org/xujia/minibase/KeyValue.java
@@ -16,8 +16,12 @@ public class KeyValue implements Comparable<KeyValue>{
      *     下述代码中：rawKey和key等价意思，对应着hbase中的key部分，但也有区别的。
      *     区别在于，在hbase中key囊括rowKey,family,qualifier的固定长度字段和变长的值字段，rowKey根据进行排序（横向划分）,
      *     但在本代码中不考虑key的内部字段划分,直接使用key进行排序了。
+     *
+     *     关于存储空间：字段划分为rawKeyLen+valueLen+rawKey+Op+sequenceId+value的可变字节数组（是序列化的数组）
+     *              但是注意除了rawKey和value是可变长度的字段外，其余都是固定长度的字段（这就需要Bytes类来转化为定长了）。
+     *
      */
-    public static final int RAW_KEY_LEN_SIZE = 4;
+    public static final int RAW_KEY_LEN_SIZE = 4; // 用于保存长度值的字段，并非key的真实长度
     public static final int VAL_LEN_SIZE = 4;
     public static final int OP_SIZE = 1;
     public static final int SEQ_ID_SIZE = 4;
@@ -80,14 +84,17 @@ public class KeyValue implements Comparable<KeyValue>{
 
     private int getRawKeyLen(){return key.length + SEQ_ID_SIZE + OP_SIZE;} // key是变长
 
-    public byte[] toBytes() throws IOException {
-        // 字节数组：rawKeyLen+valueLen+rawKey+Op+sequenceId+value
-        int rawKeyLen = getRawKeyLen();
+    public byte[] toBytes() throws IOException { // 将KeyValue对象编码到字节数组
+        /**
+         *         存储空间：字段划分为rawKeyLen+valueLen+rawKey+Op+sequenceId+value的可变字节数组。
+         *         但是注意除了rawKey和value是可变长度的字段外，其余都是固定长度的字段（这就需要Bytes类来转化为定长了）。
+         */
         int pos = 0;
-        byte[] bytes = new byte[getSerializeSize()];
+        byte[] bytes = new byte[getSerializeSize()]; // 创建存储空间
         // encode raw key length
-        byte[] rowKeyLenBytes = Bytes.toBytes(rawKeyLen);
-        System.arraycopy(rowKeyLenBytes,0,bytes,pos,RAW_KEY_LEN_SIZE); // 只去raw key部门
+        int rawKeyLen = getRawKeyLen();
+        byte[] rowKeyLenBytes = Bytes.toBytes(rawKeyLen); // 将rawKey的长度值保存到固定长度的byte数组中。
+        System.arraycopy(rowKeyLenBytes,0,bytes,pos,RAW_KEY_LEN_SIZE);
         pos += RAW_KEY_LEN_SIZE;
         // encode value length
         byte[] valueLenBytes = Bytes.toBytes(value.length);
@@ -110,20 +117,51 @@ public class KeyValue implements Comparable<KeyValue>{
     }
 
     @Override
-    public boolean equals(Object kv) {return true;}
+    public boolean equals(Object kv) {
+        if (kv == null) return false;
+        if (!(kv instanceof KeyValue) ) return false;
+        return this.compareTo((KeyValue) kv) == 0;
+    }
 
-    public int getSerializeSize(){return 0;}
+    public int getSerializeSize(){return RAW_KEY_LEN_SIZE + VAL_LEN_SIZE + getRawKeyLen() + value.length;}
 
     @Override
-    public String toString() {return null;}
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("key=").append(Bytes.toHex(this.key))
+                .append("/op=").append(this.op)
+                .append("/sequenceId=").append(this.sequenceId)
+                        .append("/value=").append((Bytes.toHex(this.value)));
+        return sb.toString();
+    }
 
-    public static org.apache.minibase.KeyValue parseFrom(byte[] bytes, int offset) throws IOException {return null;}
+    public static KeyValue parseFrom(byte[] bytes, int offset) throws IOException {
+        /**
+         *  // 从字节数组解码到KeyValue对象
+         */
+        if (bytes == null){
+            throw new IOException("buffer is null");
+        }
+        if (bytes.length <= offset + RAW_KEY_LEN_SIZE + VAL_LEN_SIZE){
+            throw new IOException("Invalid offset or bytes.len.  offset:"  + offset + ",len:" + bytes.length);
+        }
+        // Decode raw key length
+        int pos = offset;
+        int rawKeyLen = Bytes.toInt(Bytes.slice(bytes,pos,RAW_KEY_LEN_SIZE)); // 解码为int
+        pos += RAW_KEY_LEN_SIZE;
+
+        // Decode key
+
+
+    }
 
     public static org.apache.minibase.KeyValue parseFrom(byte[] bytes) throws IOException {return null;}
 
     private static class KeyValueComparator implements Comparator<org.apache.minibase.KeyValue> {
         @Override
-        public int compare(org.apache.minibase.KeyValue a, org.apache.minibase.KeyValue b) {}
+        public int compare(org.apache.minibase.KeyValue a, org.apache.minibase.KeyValue b) {
+            return 0;
+        }
     };
 
 
