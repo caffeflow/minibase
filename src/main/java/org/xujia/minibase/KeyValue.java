4155c2ddb963980c8467dc50b0b115e3b3afdfb8
betterJia <1023101057@qq.com>
2022-04-25 15:20:42 +0800

    KeyValue.java  99%

--- src/main/java/org/xujia/minibase/KeyValue.java
+++ src/main/java/org/xujia/minibase/KeyValue.java
@@ -13,18 +13,40 @@ import java.util.Objects;
  **/
 public class KeyValue implements Comparable<KeyValue>{
     /**
-     *     下述代码中：rawKey和key等价意思，对应着hbase中的key部分，但也有区别的。
-     *     区别在于，在hbase中key囊括rowKey,family,qualifier的固定长度字段和变长的值字段，rowKey根据进行排序（横向划分）,
-     *     但在本代码中不考虑key的内部字段划分,直接使用key进行排序了。
+     *     （1）在hbase的存储字节空间构成为：
+     *          keyLen(4B) + ValueLen(4B)
+     *          + RowKeyLen(2B) + RowKeyBytes(变长)
+     *          + FamilyLen(1B) + FamilyBytes(变长)
+     *          + QualifierBytes(变长)
+     *          + TimeStamp(8B)
+     *          + Type(1B)
+     *          + value
      *
-     *     关于存储空间：字段划分为rawKeyLen+valueLen+rawKey+Op+sequenceId+value的可变字节数组（是序列化的数组）
-     *              但是注意除了rawKey和value是可变长度的字段外，其余都是固定长度的字段（这就需要Bytes类来转化为定长了）。
+     *     key = rowKeyBytes + familyBytes + qualifierBytes + TimeStamp + Type
+     *     QualifierBytes变长，但是其长度可以被推断出来。
+     *     KeyValue排序依据是以ASCII码为比较，rowKeyBytes升序->familyBytes升序-> qualifierBytes升序 -> timeStamp降序 -> type升序
+     *
+     *
+     *     （2）本代码中存储空间的构成：
+     *          rawKeyLen(4B) + ValueKeyLen(4B)
+     *          + Key(变长)
+     *          + Op(1B)
+     *          + sequenceId(4B)
+     *          + Value(变长)
+     *     区别1：相比Hbase的列簇存储,这里简化了key部分，没有family和qualifier设计，使之成为列式存储。
+     *     区别2：字段排序有些不同,本代码中Op在sequenceId之前。
+     *     区别3：字段命名差异。本代码中rawKey表示[key+sequenceId+Op]部分,注意此处rawKey对应hbase中key,而此处key对应hbase中rowKey。
+     *     对keyValue排序时以ASCII码为比较，排序为: rawKey升序 -> sequenceId降序 -> Op升序
+     *
+     *     一些补充：我们设计的Bytes类提供了编码与解码功能，可以用于KeyValue对象的反/序列化。
      *
      */
     public static final int RAW_KEY_LEN_SIZE = 4; // 用于保存长度值的字段，并非key的真实长度
     public static final int VAL_LEN_SIZE = 4;
     public static final int OP_SIZE = 1;
-    public static final int SEQ_ID_SIZE = 4;
+    public static final int SEQ_ID_SIZE = 8;
+
+    public static final KeyValueComparator KV_CMP = new KeyValueComparator();
 
     private byte[] key;
     private byte[] value;
@@ -82,7 +104,7 @@ public class KeyValue implements Comparable<KeyValue>{
 
     public long getSequenceId(){return this.sequenceId;}
 
-    private int getRawKeyLen(){return key.length + SEQ_ID_SIZE + OP_SIZE;} // key是变长
+    private int getRawKeyLen(){return key.length + SEQ_ID_SIZE + OP_SIZE;}
 
     public byte[] toBytes() throws IOException { // 将KeyValue对象编码到字节数组
         /**
@@ -91,25 +113,31 @@ public class KeyValue implements Comparable<KeyValue>{
          */
         int pos = 0;
         byte[] bytes = new byte[getSerializeSize()]; // 创建存储空间
+
         // encode raw key length
         int rawKeyLen = getRawKeyLen();
-        byte[] rowKeyLenBytes = Bytes.toBytes(rawKeyLen); // 将rawKey的长度值保存到固定长度的byte数组中。
+        byte[] rowKeyLenBytes = Bytes.toBytes(rawKeyLen);
         System.arraycopy(rowKeyLenBytes,0,bytes,pos,RAW_KEY_LEN_SIZE);
         pos += RAW_KEY_LEN_SIZE;
+
         // encode value length
         byte[] valueLenBytes = Bytes.toBytes(value.length);
         System.arraycopy(valueLenBytes,0,bytes,pos,VAL_LEN_SIZE);
         pos += VAL_LEN_SIZE;
+
         // encode key
         System.arraycopy(key,0,bytes,pos,key.length);
         pos += key.length;
+
         // encode Op
         bytes[pos] = op.getCode();
         pos+=1;
+
         // encode sequenceId
         byte[] sequenceIdBytes = Bytes.toBytes(sequenceId);
         System.arraycopy(sequenceIdBytes,0,bytes,pos,sequenceIdBytes.length);
         pos += sequenceIdBytes.length;
+
         // encode value
         System.arraycopy(value,0,bytes,pos,value.length);
         pos += value.length;
@@ -135,37 +163,46 @@ public class KeyValue implements Comparable<KeyValue>{
         return sb.toString();
     }
 
-    public static KeyValue parseFrom(byte[] bytes, int offset) throws IOException {
-        /**
-         *  // 从字节数组解码到KeyValue对象
-         */
+    public static KeyValue parseFrom(byte[] bytes, int offset) throws IOException { // 从字节数组反序列化到KeyValue对象
+
         if (bytes == null){
             throw new IOException("buffer is null");
         }
         if (bytes.length <= offset + RAW_KEY_LEN_SIZE + VAL_LEN_SIZE){
             throw new IOException("Invalid offset or bytes.len.  offset:"  + offset + ",len:" + bytes.length);
         }
-        // Decode raw key length
         int pos = offset;
-        int rawKeyLen = Bytes.toInt(Bytes.slice(bytes,pos,RAW_KEY_LEN_SIZE)); // 解码为int
+
+        // Decode raw key length
+        int rawKeyLen = Bytes.toInt(Bytes.slice(bytes,pos,RAW_KEY_LEN_SIZE)); // 解码为int,以确定rawKey的长度。
         pos += RAW_KEY_LEN_SIZE;
 
+        // Decode value length
+        int valueLen = Bytes.toInt(Bytes.slice(bytes,pos,VAL_LEN_SIZE));
+        pos += VAL_LEN_SIZE;
+
         // Decode key
+        int keyLen = rawKeyLen - OP_SIZE - SEQ_ID_SIZE;  // -- 推断key的长度
+        byte[] key = Bytes.slice(bytes, pos, keyLen);
 
+        // Decode Op
+        Op op = Op.code2Op(bytes[pos]);
+        pos+=1;
 
+        // Decode SEQ_Id
+        long sequenceId = Bytes.toLong(Bytes.slice(bytes,pos,SEQ_ID_SIZE));
+        pos += SEQ_ID_SIZE;
+
+        // Decode Value
+        byte[] value = Bytes.slice(bytes, pos, valueLen);
+
+        // 构建对象
+        return create(key,value,op,sequenceId);
     }
 
-    public static org.apache.minibase.KeyValue parseFrom(byte[] bytes) throws IOException {return null;}
-
-    private static class KeyValueComparator implements Comparator<org.apache.minibase.KeyValue> {
-        @Override
-        public int compare(org.apache.minibase.KeyValue a, org.apache.minibase.KeyValue b) {
-            return 0;
-        }
-    };
-
-
-
+    public static KeyValue parseFrom(byte[] bytes) throws IOException {
+        return parseFrom(bytes,0);
+    }
 
 
     @Override
@@ -179,6 +216,15 @@ public class KeyValue implements Comparable<KeyValue>{
         return 0;
     }
 
+    private static class KeyValueComparator implements Comparator<KeyValue> {
+        @Override
+        public int compare(KeyValue o1, KeyValue o2) {
+            if (o1 == o2) return 0;
+            if (o1 == null ) return -1;
+            if (o2 == null)  return 1;
+            return o1.compareTo(o2);
+        }
+    };
 
 
 }
